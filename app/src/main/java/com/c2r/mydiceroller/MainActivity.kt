6b2885f61525b0619e32f0afc682a2f44938727c
Robinnsson Cifuentes Ramírez <robinnsson@gmail.com>
2020-12-15 01:43:47 -0500

    Se desarrolla el segundo coding challenge. Excelente ejercicio.

--- app/src/main/java/com/c2r/mydiceroller/MainActivity.kt
+++ app/src/main/java/com/c2r/mydiceroller/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.c2r.mydiceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,12 +14,54 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+
+        val countUp: Button = findViewById(R.id.count_button)
+        countUp.setOnClickListener { countUp() }
+
     }
 
     private fun rollDice() {
-        Toast.makeText(
-            this, "Button clicked",
-            Toast.LENGTH_SHORT
-        ).show()
+
+        val resultTex = findViewById<TextView>(R.id.result_text)
+        val randomInt = (1..6).random()
+        resultTex.text = randomInt.toString()
+    }
+
+    private fun countUp() {
+
+        val resultTex = findViewById<TextView>(R.id.result_text)
+        val item = resultTex.text.toString().take(1)
+        var num: Int
+
+        when (item) {
+            "1" -> {
+                num = item.toInt().plus(1)
+                resultTex.text = num.toString()
+            }
+            "2" -> {
+                num = item.toInt().plus(1)
+                resultTex.text = num.toString()
+            }
+            "3" -> {
+                num = item.toInt().plus(1)
+                resultTex.text = num.toString()
+            }
+            "4" -> {
+                num = item.toInt().plus(1)
+                resultTex.text = num.toString()
+            }
+            "5" -> {
+                num = item.toInt().plus(1)
+                resultTex.text = num.toString()
+            }
+            "6" -> {
+                resultTex.text
+            }
+            else -> resultTex.text = "1"
+
+
+        }
+
+
     }
 }
